b90c8d00bf4833b9ac1b4694c6a6eee92318fa84
Hammad Ansari <ansarihammad52@gmail.com>
2024-07-25 11:15:39 +0530

    More about Components

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ const Title = () => <h1>Namaste React Using JSX component</h1>;
 
 //JSX
 
+console.log(Title)
+
 const Heading = () => (
   <div id="container">
+    {Title()}
+    <Title />
     <Title />
     <h1>This is a functional component</h1>
   </div>
