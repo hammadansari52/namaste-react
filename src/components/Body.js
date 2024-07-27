496556bf12001613206fc450869575479e0fbfda
Hammad Ansari <ansarihammad52@gmail.com>
2024-07-27 09:12:37 +0530

    Filter restaurant feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,15 +1,32 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurants from "../utils/mockData";
+import { useState } from "react";
 
-const Body = () => (
-  <div className="body">
-    <div className="search">Search</div>
-    <div className="res-container">
-      {restaurants.map((res) => (
-        <RestaurantCard key={res.info.id} resData={res} />
-      ))}
+const Body = () => {
+  const [resList, setResList] = useState(restaurants);
+  return (
+    <div className="body">
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            console.log("button clicked");
+            const filteredRes = resList.filter(
+              (res) => res.info.avgRating >= 4.5
+            );
+            setResList(filteredRes);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
+      <div className="res-container">
+        {resList.map((res) => (
+          <RestaurantCard key={res.info.id} resData={res} />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Body;
