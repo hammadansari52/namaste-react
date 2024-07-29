37045e93d48d962b47dc9758c9daf57be44bd9bb
Hammad Ansari <ansarihammad52@gmail.com>
2024-07-29 11:22:35 +0530

    Routes Intro

--- src/App.js
+++ src/App.js
@@ -2,16 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Error from "./components/Error";
 
 //see console warning
 
-
 const AppLayout = () => (
-  <div className="app" onScroll={()=>{console.log("scrolling")}}>
+  <div
+    className="app"
+    onScroll={() => {
+      console.log("scrolling");
+    }}
+  >
     <Header />
     <Body />
   </div>
 );
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />
+  },
+  {
+    path: "/about",
+    element: <About />
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout />);
+root.render(<RouterProvider router={router} />);
