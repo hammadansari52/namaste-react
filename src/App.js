89b0f0046b3f78862ffb9df44a05bfe3b1d7bae0
Hammad Ansari <ansarihammad52@gmail.com>
2024-07-29 15:04:31 +0530

    Routes (SPA)

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import { createBrowserRouter } from "react-router-dom";
-import { RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 import About from "./components/About";
 import Error from "./components/Error";
+import Contact from "./components/Contact";
 
 //see console warning
 
@@ -17,7 +17,7 @@ const AppLayout = () => (
     }}
   >
     <Header />
-    <Body />
+    <Outlet />
   </div>
 );
 
@@ -25,12 +25,22 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <Error />
+    children: [
+      {
+        path:"/",
+        element: <Body/>
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <Error />,
   },
-  {
-    path: "/about",
-    element: <About />
-  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import { LOGO_URL } from "../utils/constants";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 let idx = 0;
 const Header = () => {
   // const btnName = "Login";
@@ -13,9 +14,9 @@ const Header = () => {
       </div>
       <div className="nav-items">
         <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
+          <li><Link to={"/"}>Home</Link></li>
+          <li><Link to={"/about"}>About</Link></li>
+          <li><Link to={"/contact"}>Contact Us</Link></li>
           <li>Cart</li>
           <li>
             <button
